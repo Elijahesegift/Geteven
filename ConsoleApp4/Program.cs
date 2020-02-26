4a350e176420162048ff96099444df7eb751f9f4
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-02-25 22:32:08 -0800

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            int[] Array = { 1, 2, 3, 4, 6, 5, 7, 8, 11 };
+            for (int i = 0; i <= Array.Length-1; i++)
+            {
+                int temp = Array[i];
+                temp %= 2;
+                if (temp == 0)
+                {
+                    Console.WriteLine(Array[i]);
+                }
+            }
+        } 
+    }
+}
